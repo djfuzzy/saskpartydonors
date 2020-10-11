8da26fe343d408b2ef7e02b2c6bbc7fd4f3d4f6e
Dillon Forsberg <djforsberg@hotmail.com>
2020-10-10 22:53:24 -0600

    Create base API

---
+++ api/Controllers/ContributorsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SaskPartyDonors.Controllers
+{
+    [ApiController]
+    [Route("api/contributions")]
+    public class ContributorsController : ControllerBase
+    {
+        private readonly ILogger<ContributorsController> _logger;
+
+        public ContributorsController(ILogger<ContributorsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Contributor Get()
+        {
+            return new Contributor
+            {
+                Id = Guid.NewGuid(),
+                Name = "Test Smith",
+                Type = "Individual",
+                Recipient = "The Party",
+                Year = 2019,
+                Amount = 123.45M
+            };
+        }
+    }
+}
---
+++ api/README.md
@@ -0,0 +1,1 @@
+# Sask Party Donors API
