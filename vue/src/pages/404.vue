112faab61e9ab2d54d6907f6fb09cca59c0967e7
Dillon Forsberg <djforsberg@hotmail.com>
2020-11-14 13:21:56 -0600

    Update 404 page

--- vue/src/pages/404.vue
+++ vue/src/pages/404.vue
@@ -1,18 +1,36 @@
 <template>
-  <main-layout>
-    <section>
-      <div class="content">
-        <p>Page not found.</p>
-      </div>
-    </section>
-  </main-layout>
+  <div class="oops">
+    <main-layout>
+      <section>
+        <div class="content has-text-centered">
+          <h1 class="title">Oops</h1>
+          <p>
+            Much like the Sask Party's responsibility to the Saskatchewan
+            public, that page seems to be missing.
+          </p>
+          <v-link href="/">Go back to home</v-link>
+        </div>
+      </section>
+    </main-layout>
+  </div>
 </template>
 
 <script>
-import MainLayout from '../layouts/Main.vue';
+import VLink from "../components/VLink";
+import MainLayout from "../layouts/Main.vue";
 export default {
   components: {
     MainLayout,
-  },
+    VLink
+  }
 };
 </script>
+
+<style scoped>
+.oops {
+  margin: 2em auto auto auto;
+  padding-left: 1em;
+  padding-right: 1em;
+  max-width: 800px;
+}
+</style>
