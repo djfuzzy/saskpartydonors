b54a0fee2acd3256c8a395ce5f4555c60f6ea11c
Dillon Forsberg <djforsberg@hotmail.com>
2020-11-13 21:29:38 -0600

    Remove ID

--- api/Services/Contributions/Dtos/ContributionByRecipientDto.cs
+++ api/Services/Contributions/Dtos/ContributionByRecipientDto.cs
@@ -1,12 +1,9 @@
-using System;
 using SaskPartyDonors.Entities;
 
 namespace SaskPartyDonors.Services.Contributions.Dtos
 {
   public class ContributionByRecipientDto
   {
-        public Guid Id { get; set; }
-
         public string ContributorName { get; set; }
 
         public ContributorType ContributorType { get; set; }
